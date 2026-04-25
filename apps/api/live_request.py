178299b0854dfb4ee85d21260adb9da57eafe1bf
Aarón Quintanilla <aquinta2006@gmail.com>
2026-04-25 12:43:47 +0200

    Fixed:Feature/api functions (#4)
    * api script made
    
    * Fixed: live_request

---
+++ apps/api/live_request.py
@@ -0,0 +1,69 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()  # Carga las variables de entorno desde el archivo .env
+
+
+API_KEY = os.getenv("SKYSCANNER_API_KEY")
+
+
+def buscar_vols(origin, destiny, year, month, day, adults, childrenages):
+   url = "https://partners.api.skyscanner.net/apiservices/v3/flights/live/search/create"
+  
+   body = {
+       "query": {
+           "market": "UK",
+           "locale": "en-GB",
+           "currency": "GBP",
+           "queryLegs": [
+           {
+               "originPlaceId": {
+                   "iata": origin,
+               },
+               "destinationPlaceId": {
+                   "iata": destiny,
+               },
+               "date": {
+                   "year": year,
+                   "month": month,
+                   "day": day
+               }
+           }
+           ],
+           "cabinClass": "CABIN_CLASS_ECONOMY",
+           "adults": adults,
+           "childrenAges": childrenages,
+           "includedCarriersIds": [],
+           "excludedCarriersIds": [],
+           "includedAgentsIds": [],
+           "excludedAgentsIds": [],
+
+
+       }
+   }
+  
+
+
+   headers = {
+       "Content-Type": "application/json",
+       "x-api-key": API_KEY
+   }
+
+
+   res = requests.post(url, json=body, headers=headers)
+
+
+   #print("Status:", res.status_code)
+   aux = res.json()
+   print(aux)
+
+
+   return res.json()
+
+
+
+
+if __name__ == "__main__":
+   buscar_vols("BCN", "TLV", 2026, 4, 25, 1, [2, 5])
